d03eab410c883ea205f99ce5fc1350014e2ce9d4
Maruen Mehana <maruen@gmail.com>
2014-09-26 12:17:52 -0300

    Dongles testers

--- bin/testCLARODongles.sh
+++ bin/testCLARODongles.sh
@@ -6,8 +6,8 @@ then
   exit 1
 fi
 
-python sendsms.py CLARO-01 +5511984388499 "$1"
+ssh atsms@186.223.215.251 -p2224  "sendsms.py CLARO-01 +5511984388499 $1"
 python sendsms.py CLARO-02 +5511984388499 "$1"
-python sendsms.py CLARO-03 +5511984388499 "$1"
+ssh atsms@186.223.215.251 -p2224  "sendsms.py CLARO-03 +5511984388499 $1"
 python sendsms.py CLARO-04 +5511984388499 "$1"
 python sendsms.py CLARO-05 +5511984388499 "$1"
