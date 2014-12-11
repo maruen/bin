5d01e0cb764d87d65a47854b394323bf9d383fb8
Maruen Mehana <maruen@gmail.com>
2014-12-11 00:54:26 -0200

    fixings

--- bin/updateHosts.sh
+++ bin/updateHosts.sh
@@ -36,14 +36,14 @@ done < ${CUR_DIR}/hosts.tmp
 
 REMOVE_TMP_FILE=`rm ${CUR_DIR}/hosts.tmp`
 REMOVE_IPV6_FILE=`rm ${CUR_DIR}/hosts.ipv6`
+NOW=$(date +"%d/%m/%Y %T")
 
 if [ "${IP_CHANGED}" == "1" ]  && [ "${CUR_IP}" != "" ];
 then
-	echo "The IP of $1 has changed from ${PREVIOUS_IP} to ${CUR_IP}"
-	echo "Uploading the new hosts file to root@96.126.114.91:/etc/hosts"	
+	echo "${NOW}: The IP of $1 has changed from ${PREVIOUS_IP} to ${CUR_IP}, uploading the new hosts file to root@96.126.114.91:/etc/hosts"
 	REPLACE_HOSTS=`scp ${CUR_DIR}/hosts root@96.126.114.91:/etc/hosts`
 else
-	echo "There was no changing of IP, nothing done!"	
+	echo "${NOW}: There was no changing of IP, nothing done!"	
 fi
 
 REMOVE_HOSTS_FILE=`rm ${CUR_DIR}/hosts`
