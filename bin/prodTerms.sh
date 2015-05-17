75c557f5cbe95d5ec2a44e5c3abc3ac42b3956c1
Maruen Mehana <maruen@gmail.com>
2015-05-17 01:38:17 -0300

    new updates

--- bin/prodTerms.sh
+++ bin/prodTerms.sh
@@ -1,6 +1,6 @@
-gnome-terminal --tab-with-profile=CLUSTER-01						\
-	       		--tab-with-profile=CLUSTER-02						\
-		 	      --tab-with-profile=CLUSTER-03 					\
+gnome-terminal --tab-with-profile=CLUSTER-02						\
+	       		--tab-with-profile=CLUSTER-03						\
+		 	      --tab-with-profile=CLUSTER-04 					\
 	      		--tab-with-profile=PROD-MYSQL-ATSMS	 			\
 	      		--tab-with-profile=PROD-MYSQL-PORTABILITY	 	\
 			      --tab-with-profile=PROD-PLAYSERVER				\
